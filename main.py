76a125f3f53a297d6c0f0434ff875f38f9419264
Parsa Sadri <p_sadri_s@yahoo.com>
2021-02-19 18:35:01 +0330

    fix bug, after the second news keeps repeating

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def get_news():
 				break
 			last_news = news
 			send_notification(news)
+			break
 		if "Hard Fork" in news:
 			if last_news == "":
 				last_news = news
@@ -58,6 +59,7 @@ def get_news():
 				break
 			last_news = news
 			send_notification(news)
+			break
 
 
 last_news = ""
