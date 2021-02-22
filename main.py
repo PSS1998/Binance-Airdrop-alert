ac9a73836c591ae17e75650d8dec53583077efc4
Parsa Sadri <p_sadri_s@yahoo.com>
2021-02-22 20:53:46 +0330

    Add buying feature

--- main.py
+++ main.py
@@ -5,11 +5,31 @@ import json
 import schedule
 import bs4 as bs
 import telegram
+from binance.client import Client
+from binance.enums import *
+import math
+
+import config
 
 
 
 TOKEN='Insert your telgram token here'
 
+def buy_symbol(symbol):
+	amount = 0.0005
+	market = symbol+"BTC"
+	client = Client(config.api_key, config.api_secret)
+	depth = client.get_order_book(symbol=market, limit=5)
+	lastBid = float(depth['bids'][0][0]) #last buy price (bid)
+	quantity = (amount / lastBid)
+	info = client.get_exchange_info()
+	symbol_info = [markett for markett in info['symbols'] if markett['symbol'] == market][0]
+	symbol_info['filters'] = {item['filterType']: item for item in symbol_info['filters']}
+	filters = symbol_info['filters']
+	stepSize = float(filters['LOT_SIZE']['stepSize'])
+	quantity = float(stepSize * math.floor(float(quantity)/stepSize))
+	order = client.order_market_buy(symbol=market, quantity=quantity) 
+	return order
 
 def send_notification(text):
 	global bot
@@ -51,6 +71,11 @@ def get_news():
 				break
 			last_news = news
 			send_notification(news)
+			try:
+				symbol = text.split("(")[-1].split(")")[0]
+				result = buy_symbol(symbol)
+			except Exception as e:
+				print(e)
 			break
 		if "Hard Fork" in news:
 			if last_news == "":
